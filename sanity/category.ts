15de5e74c32a6866c7a6fe343c14d1ba051797ee
wasimiqbal1 <wasim_iqbal@msn.com>
2023-06-05 00:17:55 +0500

    Category add in Sanity

---
+++ sanity/category.ts
@@ -0,0 +1,11 @@
+
+export const category = {
+    name : "category",
+    title : "Category",
+    type:"document",
+    fields: [{
+        name:"name",
+        title:"Category Name",
+        type:"string"
+    }]
+}
